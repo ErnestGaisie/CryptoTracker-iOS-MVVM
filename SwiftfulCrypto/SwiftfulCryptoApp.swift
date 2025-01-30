9af61c53bdc5f520f93d3a506c321c6a27e2e41e
Ernest Gaise <ernestyawgaisie@gmail.com>
2025-01-29 23:06:08 -0500

    Initial Commit

---
+++ SwiftfulCrypto/SwiftfulCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulCryptoApp.swift
+//  SwiftfulCrypto
+//
+//  Created by Ernest Gaisie on 29/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
