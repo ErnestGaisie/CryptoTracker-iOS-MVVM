fd87c03e0c7b89a9b23989081fa2ef664c1066bf
Ernest Gaise <ernestyawgaisie@gmail.com>
2025-01-30 23:10:08 -0500

    Created HomeView and a navigation header

---
+++ SwiftfulCrypto/Core/Home/Views/HomeView.swift
@@ -0,0 +1,61 @@
+//
+//  HomeView.swift
+//  SwiftfulCrypto
+//
+//  Created by Ernest Gaisie on 30/01/2025.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    @State private var showPortfolio: Bool = false
+    
+    var body: some View {
+        ZStack {
+            // background layer
+            Color.theme.background.ignoresSafeArea()
+            
+            // content layer
+            VStack {
+                homeHeader
+                Spacer(minLength: 0)
+            }
+        }
+    }
+}
+
+#Preview {
+    NavigationView {
+        HomeView().navigationBarHidden(true)
+    }
+}
+
+extension HomeView {
+    
+    private var homeHeader: some View {
+        HStack {
+            CircleButtonView(iconName: showPortfolio ? "plus" : "info")
+                .animation(.none, value: showPortfolio)
+                .background(
+                    CircleButtonAnimationView(animate: $showPortfolio)
+                )
+            Spacer()
+            Text(showPortfolio ? "Portolio" : "Live Prices")
+                .font(.headline)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.theme.accent)
+                .animation(.none, value: showPortfolio)
+            Spacer()
+            CircleButtonView(iconName: "chevron.right")
+                .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
+                .onTapGesture {
+                    withAnimation(.spring()){
+                        showPortfolio.toggle()
+                    }
+                }
+        }
+        .padding(.horizontal)
+    }
+    
+}
